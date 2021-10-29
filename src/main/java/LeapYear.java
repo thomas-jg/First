91154f358deb040b7e8eef6128038952c1490744
thomas-jg <thomasjg@hiof.no>
2021-10-29 15:20:27 +0200

    commit test

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,5 +1,5 @@
 public class LeapYear {
     public boolean isLeapYear(int year) {
-        return ((year % 5 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
     }
 }
